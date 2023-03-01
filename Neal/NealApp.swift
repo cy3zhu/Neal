bba93772af4bf0b61bdbcb730541eba7fbe38905
Christopher Zhu <zhuchristopher@hotmail.com>
2023-02-28 22:16:57 -0500

    Initial Commit

---
+++ Neal/NealApp.swift
@@ -0,0 +1,17 @@
+//
+//  NealApp.swift
+//  Neal
+//
+//  Created by ChrisZ on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct NealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
